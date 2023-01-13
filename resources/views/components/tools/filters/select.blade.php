ac0148bc34528cfadd83ef49547b31b1d49f2a78
Joe <joe@lowerrocklabs.com>
2023-01-13 08:09:33 +0000

    Fixes for MultiSelectDropdown

--- resources/views/components/tools/filters/select.blade.php
+++ resources/views/components/tools/filters/select.blade.php
@@ -11,7 +11,7 @@
             class="block w-full border-gray-300 rounded-md shadow-sm transition duration-150 ease-in-out focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-800 dark:text-white dark:border-gray-600"
         >
             @if ($filter->getFirstOption() != "")
-            <option value="">{{ $filter->getFirstOption()}}</option>
+                <option value="">{{ $filter->getFirstOption()}}</option>
             @endif
             @foreach($filter->getOptions() as $key => $value)
                 @if (is_iterable($value))
--- src/Views/Filters/MultiSelectDropdownFilter.php
+++ src/Views/Filters/MultiSelectDropdownFilter.php
@@ -16,6 +16,18 @@ class MultiSelectDropdownFilter extends Filter
         return $this;
     }
 
+    public function setFirstOption(string $firstOption): SelectFilter
+    {
+        $this->firstOption = $firstOption;
+
+        return $this;
+    }
+
+    public function getFirstOption(): string
+    {
+        return $this->firstOption;
+    }
+
     public function getOptions(): array
     {
         return $this->options;

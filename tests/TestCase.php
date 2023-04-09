3d718df381ef9c14e56d86cda2b4d8cead0cbfef
lrljoe <joe@lowerrocklabs.com>
2023-04-09 01:29:21 +0100

    Test Tweaks

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -88,10 +88,12 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app): void
     {
         config()->set('app.key', Encrypter::generateKey(config('app.cipher')));
+        config()->set('app.env', 'testing');
         config()->set('cache.default', 'array');
         config()->set('view.cache', false);
 
         
+        $app['config']->set('app.env', 'testing');
         $app['config']->set('database.default', 'sqlite');
         $app['config']->set('cache.default', 'array');
         $app['config']->set('view.cache', false);

3ab7d4732b6b7dc90ded243fc11914947a664d5b
lrljoe <lrljoe@users.noreply.github.com>
2023-09-07 01:01:51 +0000

    Fix styling

--- src/Views/Filters/LivewireComponentFilter.php
+++ src/Views/Filters/LivewireComponentFilter.php
@@ -2,12 +2,13 @@
 
 namespace Rappasoft\LaravelLivewireTables\Views\Filters;
 
-use Rappasoft\LaravelLivewireTables\Views\Filter;
 use Illuminate\Support\Str;
+use Rappasoft\LaravelLivewireTables\Views\Filter;
 
 class LivewireComponentFilter extends Filter
 {
     public string $filterComponentPath = '';
+
     public array $filterComponentOptions = [];
 
     public function validate(mixed $value): mixed
@@ -46,39 +47,31 @@ class LivewireComponentFilter extends Filter
     {
         $this->config = $config;
 
-        if ($this->filterComponentPath == '' && $this->hasConfig('filterComponentPath'))
-        {
+        if ($this->filterComponentPath == '' && $this->hasConfig('filterComponentPath')) {
             $this->setFilterComponentPath($this->getConfig('filterComponentPath'));
         }
 
-        if ($this->filterComponentOptions == [] && $this->hasConfig('filterComponentOptions'))
-        {
+        if ($this->filterComponentOptions == [] && $this->hasConfig('filterComponentOptions')) {
             $this->setFilterComponentOptions($this->getConfig('filterComponentOptions'));
         }
 
         return $this;
     }
 
-
-    
-    
-
     public function render(string $filterLayout, string $tableName, bool $isTailwind, bool $isBootstrap4, bool $isBootstrap5): string|\Illuminate\Contracts\Foundation\Application|\Illuminate\View\View|\Illuminate\View\Factory
     {
-        return view('livewire-tables::components.tools.filters.livewire-component-filter',  [
+        return view('livewire-tables::components.tools.filters.livewire-component-filter', [
             'filterLayout' => $filterLayout,
             'tableName' => $tableName,
             'isTailwind' => $isTailwind,
             'isBootstrap' => ($isBootstrap4 || $isBootstrap5),
             'isBootstrap4' => $isBootstrap4,
             'isBootstrap5' => $isBootstrap5,
-            'wirekey' => $tableName . '-filter-'. $this->getKey() . ($this->hasCustomPosition() ? '-'.$this->getCustomPosition() : ''),
+            'wirekey' => $tableName.'-filter-'.$this->getKey().($this->hasCustomPosition() ? '-'.$this->getCustomPosition() : ''),
             'livewireComponent' => $this->filterComponentPath,
             'modelableValue' => 'filterComponents.'.$this->getKey(),
             ...$this->filterComponentOptions,
         ]);
-
-
 
     }
 }

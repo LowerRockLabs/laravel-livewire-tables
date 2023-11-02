379f67c4211df6a344c8d1131c7d28ff975a0033
Joe <104938042+lrljoe@users.noreply.github.com>
2023-11-02 17:45:18 +0000

    Fix Class Array

--- resources/views/components/table/th/plain.blade.php
+++ resources/views/components/table/th/plain.blade.php
@@ -4,8 +4,8 @@
 <th x-cloak scope="col"
     {{ $attributes
         ->merge($customAttributes)
-        ->class('table-cell px-3 py-2 md:px-6 md:py-3 text-center md:text-left bg-gray-50 dark:bg-gray-800 laravel-livewire-tables-reorderingMinimised' => $component->isTailwind())
-        ->class('laravel-livewire-tables-reorderingMinimised' => $component->isBootstrap())
+        ->class(['table-cell px-3 py-2 md:px-6 md:py-3 text-center md:text-left bg-gray-50 dark:bg-gray-800 laravel-livewire-tables-reorderingMinimised' => $component->isTailwind()])
+        ->class(['laravel-livewire-tables-reorderingMinimised' => $component->isBootstrap()])
     }}
     @if($hideUntilReorder) :class="!reorderDisplayColumn && 'w-0 p-0 hidden'" @endif
 >

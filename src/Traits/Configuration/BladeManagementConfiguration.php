b1d19d7433a6f2e9424129fb92afb28767a19f55
lrljoe <lrljoe@users.noreply.github.com>
2023-11-18 18:35:10 +0000

    Fix styling

--- src/Traits/Configuration/BladeManagementConfiguration.php
+++ src/Traits/Configuration/BladeManagementConfiguration.php
@@ -4,15 +4,14 @@ namespace Rappasoft\LaravelLivewireTables\Traits\Configuration;
 
 trait BladeManagementConfiguration
 {
-
     public function setCustomBladeOfflinePath(string $offlineBladePath): self
     {
         $this->offlineBladePath = $offlineBladePath;
-        
+
         return $this;
 
     }
-    
+
     public function setCustomBladeDebugPath(string $debugBladePath): self
     {
         $this->debugBladePath = $debugBladePath;

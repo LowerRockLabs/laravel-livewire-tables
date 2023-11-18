dd5be558739c739f4259d66893e792c8199996b9
Joe <104938042+lrljoe@users.noreply.github.com>
2023-11-18 18:34:49 +0000

    Return this

--- src/Traits/Configuration/BladeManagementConfiguration.php
+++ src/Traits/Configuration/BladeManagementConfiguration.php
@@ -12,13 +12,8 @@ trait BladeManagementConfiguration
         return $this;
 
     }
-<<<<<<< HEAD
     
     public function setCustomBladeDebugPath(string $debugBladePath): self
-=======
-
-    public function setCustomBladeDebugPath(string $debugBladePath)
->>>>>>> 20b71f6d2b3166cc1ecacf4b34ce0c661b70c5b1
     {
         $this->debugBladePath = $debugBladePath;
 

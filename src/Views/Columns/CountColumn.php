d91cd636dd8df4af3e3a618c9eb0e8d1cfc5fce9
lrljoe <lrljoe@users.noreply.github.com>
2024-02-18 05:56:50 +0000

    Fix styling

--- src/Views/Columns/CountColumn.php
+++ src/Views/Columns/CountColumn.php
@@ -13,9 +13,11 @@ class CountColumn extends Column
     public function __construct(string $title, ?string $from = null)
     {
         parent::__construct($title, $from);
-        $this->label(function ($row, Column $column) use ($from) { return $row->{$from.'_count'};} );
+        $this->label(function ($row, Column $column) use ($from) {
+            return $row->{$from.'_count'};
+        });
         $this->sortable(
-            fn($query, string $direction) => $query->orderBy($from.'_count', $direction)
+            fn ($query, string $direction) => $query->orderBy($from.'_count', $direction)
         );
         $this->countField = $from;
         $this->countColumn = true;

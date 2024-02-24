9ac7ac1cc7b6682fc7a16c2e07e771d994e623c7
Joe <104938042+lrljoe@users.noreply.github.com>
2024-02-24 07:16:17 +0000

    Add CursorPointer, Merge Defaults

--- resources/views/components/tools/actions.blade.php
+++ resources/views/components/tools/actions.blade.php
@@ -5,7 +5,7 @@
         @continue($action->isHidden())
         <a {{
             $attributes->merge($action->getAttributes())
-            ->class(['inline-flex justify-center px-4 py-2 text-sm font-medium rounded-md border shadow-sm focus:ring focus:ring-opacity-50' => ($action->getAttributes()['default'] ?? true) && ($action->getAttributes()['default-styling'] ?? true)])
+            ->class(['cursor-pointer inline-flex justify-center px-4 py-2 text-sm font-medium rounded-md border shadow-sm focus:ring focus:ring-opacity-50' => ($action->getAttributes()['default'] ?? true) && ($action->getAttributes()['default-styling'] ?? true)])
             ->class(['text-gray-700 bg-white border-gray-300 hover:bg-gray-50 focus:border-indigo-300 focus:ring-indigo-200 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:hover:bg-gray-600' => ($action->getAttributes()['default'] ?? true) && ($action->getAttributes()['default-colors'] ?? true)])
             ->class(['btn btn-sm btn-success' => ! $action->hasClass()])
         }}

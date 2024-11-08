0c292f0eb2d16535aa9a1ebc025be7a232bb32cf
Joe <104938042+lrljoe@users.noreply.github.com>
2024-11-08 04:57:36 +0000

    Initial Commit

--- src/LaravelLivewireTablesServiceProvider.php
+++ src/LaravelLivewireTablesServiceProvider.php
@@ -25,15 +25,12 @@ class LaravelLivewireTablesServiceProvider extends ServiceProvider
         );
 
         // Load Default Translations
-        $this->loadJsonTranslationsFrom(
-            __DIR__.'/../resources/lang'
-        );
-
-        // Override if Published
-        $this->loadJsonTranslationsFrom(
-            $this->app->langPath('vendor/livewire-tables')
-        );
-
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'livewire-tables');
+ 
+        $this->publishes([
+            __DIR__.'/../resources/lang' => $this->app->langPath('livewire-tables'),
+        ]);
+    
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'livewire-tables');
 
         $this->consoleCommands();
